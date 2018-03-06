75a992f388baebe72991ddbc76fe577110df7023
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-06 22:02:08 +0800

    Done: rails g model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306140008) do
+ActiveRecord::Schema.define(version: 20180306140130) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "gender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genders", force: :cascade do |t|
     t.string "name"
