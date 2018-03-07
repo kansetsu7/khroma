cad8b347fb6981298bc9e18c06f00fd119d49f09
binyaha <yahabin@gmail.com>
2018-03-07 08:49:49 +0800

    categories interface: routes&basic MVC

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @gender = Gender.find(params[:gender_id])
+    @categories = @gender.categories
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  #genders interface routes
   resources :genders do
     resources :categories, only: [:index]
+  end
+
+  #categories interface routes
+  resources :categories do
+    resources :types, only: [:index]
   end
   
 end
