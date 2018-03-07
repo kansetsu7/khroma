e7c7c936fe113198d5f85cefef88bb1aa0dbc194
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-07 15:50:03 +0800

    Done: read json from api into array

--- mechanize/lativ/color_extract.rb
+++ mechanize/lativ/color_extract.rb
@@ -14,7 +14,20 @@ def get_color(image_url, api_key, api_secret)
   #   call_api(image_url, api_key, api_secret)
   # end
 
-  call_api(image_url, api_key, api_secret)
+  # test_method
+
+  result_arr = call_api(image_url, api_key, api_secret)
+  result_arr.each_with_index do |v, i|
+    puts "#{i}, #{v}"
+  end
+end
+
+def test_method
+  a =Array.new(3)
+  a[1] = nil
+  puts a[0]
+  puts a[0].nil?
+  puts a[1].nil?
 end
 
 def call_api(image_url, api_key, api_secret)
@@ -29,31 +42,57 @@ def call_api(image_url, api_key, api_secret)
     puts "success"
 
     info = data["results"][0]["info"]
+
+    # ==== background_colors = result_arr[0~11] ====
     background_colors = info["background_colors"]
-    puts "==== background_colors ===="
-    background_colors.each_with_index do |color, i|
-      puts "// ==== #{i} ===="
-      puts "percentage: #{color['percentage']}%"
-      puts "rgb: [#{color['r']}, #{color['g']}, #{color['b']}]"
-      puts "html_code: #{color['html_code']}"
-      puts "closest_palette_color_html_code: #{color['closest_palette_color_html_code']}"
-      puts "closest_palette_color: #{color['closest_palette_color']}"      
+    for i in 0..2 do
+      unless background_colors[i].nil?  # 有值
+        result_arr[i*4]   = background_colors[i]['html_code']
+        result_arr[i*4+1] = background_colors[i]['closest_palette_color_html_code']
+        result_arr[i*4+2] = background_colors[i]['closest_palette_color']
+        result_arr[i*4+3] = background_colors[i]['percentage']
+      end
     end
 
-    puts "==== color_variance & object_percentage ===="
-    puts "color_variance: #{info['color_variance']}"
-    puts "object_percentage: #{info['object_percentage']}"
+    # background_colors.each_with_index do |color, i|
+    #   puts "// ==== #{i} ===="
+    #   puts "percentage: #{color['percentage']}%"
+    #   puts "rgb: [#{color['r']}, #{color['g']}, #{color['b']}]"
+    #   puts "html_code: #{color['html_code']}"
+    #   puts "closest_palette_color_html_code: #{color['closest_palette_color_html_code']}"
+    #   puts "closest_palette_color: #{color['closest_palette_color']}"      
+    # end
 
-    puts "\n==== image_colors ===="
+    result_arr[12] = info['object_percentage']
+
+    # puts "==== color_variance & object_percentage ===="
+    # puts "color_variance: #{info['color_variance']}"
+    # puts "object_percentage: #{info['object_percentage']}"
+
+    # ==== image_colors = result_arr[13~32] ====
     image_colors = info["image_colors"]
-    image_colors.each_with_index do |color, i|
-      puts "// ==== #{i} ===="
-      puts "percent: #{color['percent']}%"
-      puts "rgb: [#{color['r']}, #{color['g']}, #{color['b']}]"
-      puts "html_code: #{color['html_code']}"
-      puts "closest_palette_color_html_code: #{color['closest_palette_color_html_code']}"
-      puts "closest_palette_color: #{color['closest_palette_color']}"
+    for i in 0..4 do
+      unless image_colors[i].nil?  # 有值
+        # puts image_colors[i]['html_code']
+        # puts image_colors[i]['closest_palette_color_html_code']
+        # puts image_colors[i]['closest_palette_color']
+        # puts image_colors[i]['percent']
+        result_arr[i*4+13] = image_colors[i]['html_code']
+        result_arr[i*4+14] = image_colors[i]['closest_palette_color_html_code']
+        result_arr[i*4+15] = image_colors[i]['closest_palette_color']
+        result_arr[i*4+16] = image_colors[i]['percent']
+      end
     end
+    
+    # image_colors.each_with_index do |color, i|
+    #   puts "// ==== #{i} ===="
+    #   puts "percent: #{color['percent']}%"
+    #   puts "rgb: [#{color['r']}, #{color['g']}, #{color['b']}]"
+    #   puts "html_code: #{color['html_code']}"
+    #   puts "closest_palette_color_html_code: #{color['closest_palette_color_html_code']}"
+    #   puts "closest_palette_color: #{color['closest_palette_color']}"
+    # end
+    result_arr
   end
   
 end
