fd8fd2bd207f1077f836e080471ce941d4551ff5
binyaha <yahabin@gmail.com>
2018-03-08 08:27:26 +0800

    backstage products CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,40 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @style = Style.find(params[:style_id])
+    @products = @style.products
+    @product = Product.new
+
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.style_id = params[:style_id]
+    @product.save
+    redirect_to admin_style_products_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @style = Style.find(params[:style_id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    @product.save
+    redirect_to admin_style_products_path
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_style_products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :style_id)
+  end
+  
 end
