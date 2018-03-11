73ccf5f6abf8eb3e4634caafd5dbcf91026d9d73
binyaha <yahabin@gmail.com>
2018-03-11 17:49:34 +0800

    create category model relateion to products & view use recommend partial

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,8 @@
 class CategoriesController < ApplicationController
 
   def index
-    @gender = Gender.find(params[:gender_id])
-    @categories = @gender.categories
+    gender = Gender.find(params[:gender_id])
+    @categories = gender.categories
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :types
   belongs_to :gender
+  has_many :styles, through: :types
+  has_many :products, through: :styles
 end
