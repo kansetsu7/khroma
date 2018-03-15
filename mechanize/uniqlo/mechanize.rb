3637af02101179d215c2730032c298d1ab1f49b9
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-15 21:03:51 +0800

    tmp: commit before checkout

--- mechanize/uniqlo/mechanize.rb
+++ mechanize/uniqlo/mechanize.rb
@@ -114,10 +114,10 @@ def get_uniqlo_styles
   styles_arr.each_with_index do |gender, i|
     next unless i == 0  # 先抓men (i==0)
     gender.each_with_index do |category, j|
-      # next unless j == 0
+      next unless j == 0
       puts "get styles for gender#{i}'s category#{j}..."
       category.each_with_index do |type, k|
-        # next unless k == 0
+        next unless k == 0
         puts "goto #{types_link_arr[i][j][k]}"
         styles_of_category_arr = get_uniqlo_styles_of_type(types_link_arr[i][j][k])
         styles_of_category_arr.each_with_index do |style, l|
@@ -501,6 +501,6 @@ end
 # ok!
 # get_uniqlo_data(true)
 # get_uniqlo_types(true)
-#- get_uniqlo_styles
+get_uniqlo_styles
 # read_styles
 # get_uniqlo_products
