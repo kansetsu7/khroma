5c45fd68fec49fdf30c605517f8b50a81c514632
smallwordWISH <word714cs@gmail.com>
2018-03-28 01:42:09 +0800

    complete styles infinite_scroll

--- app/assets/javascripts/channels/styles.js
+++ app/assets/javascripts/channels/styles.js
@@ -1,8 +1,47 @@
-$(document).on('turbolinks:load', function(){
+$(window).on("scroll", debounce(function(){
+  if (($(window).height() + $(window).scrollTop()) >= $(document).height()) {
 
+    var nextSelector = $('nav.pagination a[rel=next]').attr('href');
+    if ( typeof nextSelector === 'undefined') {
+      $('#styles-spinner').html('<em>沒有更多商品</em>')
+      return
+    }
+
+    $.ajax({
+      url: nextSelector,
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        $('.styles-panel').append(data['html']);
+        $('nav.pagination').replaceWith(data['paginateHtml']);  
+      }
+    });
+  }
+}));
+
+$(document).on('turbolinks:load', function(){
   $('.product-color-panel a').on('click', function(){
     $(this).parent().children('a').removeClass('color-unlocked');
     $(this).parent().children('a').removeClass('color-locked');
     $(this).addClass('color-locked');
   });
 });
+
+
+// lodash debounce function https://lodash.com/docs/4.17.5#debounce
+
+function debounce(func, wait = 500, immediate = false) {
+  var timeout;
+  return function() {
+      var context = this,
+          args = arguments;
+      var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+      }
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+  }
+}
--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -3,18 +3,22 @@ class StylesController < ApplicationController
 
   def index
     @styles = Style.where(type_id: params[:type_id]).includes(:products, :colors).order(created_at: :desc).page(params[:page]).per(20)
+    render_json
   end
 
   def price_order_asc
-    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.price ASC")
+    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.price ASC").page(params[:page]).per(20)
+    render_json
   end
   
   def price_order_desc
-    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.price DESC")
+    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.price DESC").page(params[:page]).per(20)
+    render_json
   end
 
   def brand_order
-    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.brand ASC").order("products.price ASC")
+    @styles = Style.where(type_id: params[:type_id]).includes(products: :color).includes(:colors).order("products.brand ASC").order("products.price ASC").page(params[:page]).per(20)
+    render_json
   end
 
   def change_color
@@ -26,4 +30,13 @@ class StylesController < ApplicationController
     def set_type
       @type = Type.find(params[:type_id])
     end
+
+    def render_json
+    if params[:page].to_i > 1
+      render json: { 
+        html: render_to_string(partial: "shared/product_sm", collection: @styles, as: :style),
+        paginateHtml: render_to_string(partial: "shared/paginate", locals: { styles: @styles} )
+      }
+    end
+  end
 end
