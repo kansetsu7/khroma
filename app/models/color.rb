35dfb0482fb3c5aeb36b8746c241b90a51ed20b1
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-27 20:55:05 +0800

    Done: set up relation between color and virtual_product

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,5 @@
 class Color < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, :optional => true  # optional允許不設定
   belongs_to :hue_level  
+  belongs_to :virtual_product, :optional => true  # optional允許不設定
 end
