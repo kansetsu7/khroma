a9450fbbc432c1d846f470bca8ed59306a46ee1d
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-27 22:28:30 +0800

    Done: set up relation for outfit

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,4 +1,7 @@
 class Outfit < ApplicationRecord
   belongs_to :celebrity
   has_many :outfit_principle_colors
+  has_many :outfit_clothings
+  has_many :products, through: :outfit_clothings
+  has_many :virtual_products, through: :outfit_clothings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180327124341) do
 
-  create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "gender_id"
