bb799f6e854418da537f59b91a25194bb6db2b8b
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-28 11:05:19 +0800

    Done: set up relation for outfit

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -4,4 +4,6 @@ class Outfit < ApplicationRecord
   has_many :outfit_clothings
   has_many :products, through: :outfit_clothings
   has_many :virtual_products, through: :outfit_clothings
+  has_many :product_colors, through: :products, foreign_key: :product_id, source: :color
+  has_many :virtual_product_colors, through: :virtual_products, foreign_key: :virtual_product_id, source: :color
 end
