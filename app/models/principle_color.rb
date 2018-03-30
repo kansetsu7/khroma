c1f2f251a2754e2bec5f777487bfca88b83b964e
Kansetsu Kuo <kansetsu4@gmail.com>
2018-03-30 09:28:48 +0800

    Done: add associations

--- app/models/principle_color.rb
+++ app/models/principle_color.rb
@@ -5,4 +5,5 @@ class PrincipleColor < ApplicationRecord
   belongs_to :option1_hue_level, class_name: "HueLevel", foreign_key: "hue_option1", :optional => true  # optional允許hue_match2不設定
   belongs_to :option2_hue_level, class_name: "HueLevel", foreign_key: "hue_option2", :optional => true  # optional允許hue_match2不設定
   has_many   :outfit_principle_colors
+  has_many   :outfits, through: :outfit_principle_colors
 end
