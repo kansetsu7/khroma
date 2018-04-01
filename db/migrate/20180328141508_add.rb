29773bc2568bead43fc83b7e2aeecc8d5e98adc0
binyaha <yahabin@gmail.com>
2018-04-01 11:36:44 +0800

    fix migration repeat column

--- db/migrate/20180328141508_add.rb
+++ db/migrate/20180328141508_add.rb
@@ -2,6 +2,6 @@ class Add < ActiveRecord::Migration[5.1]
   def change
     add_column :principle_colors, :hue_option2, :integer
     rename_column :principle_colors, :hue_match2, :hue_option1
-    rename_column :principle_colors, :hue_match1, :hue_match1
+    
   end
 end
