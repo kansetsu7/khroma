26db9e3667e521512935235ba4ff0bf0af6d6c5c
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-03 14:56:04 +0800

    Done: rails g migration add_image_to_image_colors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331085017) do
+ActiveRecord::Schema.define(version: 20180403065346) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -85,11 +85,11 @@ ActiveRecord::Schema.define(version: 20180331085017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hue_option2", default: -1
+    t.string "image"
   end
 
   create_table "principles", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
