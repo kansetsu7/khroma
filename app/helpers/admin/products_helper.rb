a80f142c0c69341a73a70c16b08a15711a336af5
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-06 22:24:57 +0800

    Done: add image and color information for admin/product

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,14 @@
 module Admin::ProductsHelper
+
+  def show_product_attribute(value)
+    return value unless value.is_a? String
+    if value.include?('jpg')
+      return image_tag(value, :class => "table-product-img")  
+    elsif value.include?('http')
+      return link_to(value, value, target: :_blank)
+    end
+    
+    value
+  end
+
 end
